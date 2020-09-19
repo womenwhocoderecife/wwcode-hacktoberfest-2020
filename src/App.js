3a210b8e2d2f8bb0deff12ffef3ae1587209cefa
simoneas02 <simoneas02@hotmail.com>
2020-09-19 18:17:01 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => <h1>WWCode Recife in the Hacktoberfest 2020 </h1>;
 
 export default App;
