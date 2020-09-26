64e1f9bea15ae3d6476631fdc69d206c48ea2d7c
simoneas02 <simoneas02@hotmail.com>
2020-09-26 17:17:50 -0300

    add antialiased font and remove the orveflorw golbal setting

--- src/assets/styles/GlobalStyles.js
+++ src/assets/styles/GlobalStyles.js
@@ -1,7 +1,7 @@
-import { createGlobalStyle } from "styled-components/macro";
+import { createGlobalStyle } from 'styled-components/macro';
 
-import { fonts } from "./typography";
-import { colors } from "./colors";
+import { fonts } from './typography';
+import { colors } from './colors';
 
 const GlobalStyles = createGlobalStyle`
   html, body, #root {
@@ -12,8 +12,9 @@ const GlobalStyles = createGlobalStyle`
     display: flex;
     flex-direction: column;
     align-items: center;
-    overflow-y: scroll;
     scroll-behavior: smooth;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 
   html { 
