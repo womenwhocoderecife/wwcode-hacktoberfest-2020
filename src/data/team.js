9793b66679cc5b91c4c6ffd0be4d2c9d05438e39
Geysa Priscila <geysapriscilasz@gmail.com>
2020-10-03 17:27:00 -0300

    Add link para o linkedin.

--- src/data/team.js
+++ src/data/team.js
@@ -125,7 +125,7 @@ export default [
         description:
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore dolore magna aliqua. Ut enimad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         gitHub: 'https://github.com/GeysaPriscila',
-        linkedIn: '',
+        linkedIn: 'https://www.linkedin.com/in/geysa-priscila/',
       }
     ],
   },
