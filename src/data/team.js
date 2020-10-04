826d56ece15f1a77751ae9208c13dd0ad5dbf4e3
camilars <camila.rs122@gmail.com>
2020-10-04 11:56:48 -0300

    Adicionado descrição.

--- src/data/team.js
+++ src/data/team.js
@@ -297,7 +297,7 @@ export default [
       {
         photo:'https://avatars2.githubusercontent.com/u/37213288?s=400&u=6334fe21c0c03fb0e7e911abc7d72068f9871327&v=4',
         name: 'Camila Silva',
-        description:'',
+        description:'Mãe de duas, desenvolvedora Front-end, entusiasta por tecnologia e que anseia por conhecimento. Adoro assistir séries e ficar criando teorias sobre elas.',
         gitHub: '',
         linkedIn: '',
       }
