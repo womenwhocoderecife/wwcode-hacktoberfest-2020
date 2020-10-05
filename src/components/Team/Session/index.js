b8851323ad1de7f083038ab29df0e5ca19d9a141
Karen Gomes <kngs@ic.ufal.br>
2020-10-05 01:12:17 -0300

    refactor(session): put an implicit return instead of an explicit one
    #129

--- src/components/Team/Session/index.js
+++ src/components/Team/Session/index.js
@@ -16,33 +16,30 @@ import {
   Icon,
 } from '../../../shared-ui/Typography';
 
-const Session = ({ data, title = 'Directors', subtitle }) => {
-
-  return (
-    <Content>
-      <Title>{title}</Title>
-      <Function>{subtitle}</Function>
-      <Cards>
-        {data &&
-          data.map(user => (
-            <Card key={Math.random()}>
-              <Photo src={user.photo} alt={user.name} />
-              <Name>{user.name}</Name>
-              <Participation>{title.slice(0, -1)}</Participation>
-              <Description>{user.description}</Description>
-              <SocialContext>
-                <Social href={user.gitHub} target="_blank">
-                  <Icon src={GitHub} alt="Icon GitHub" />
-                </Social>
-                <Social href={user.linkedIn} target="_blank">
-                  <Icon src={Linkedin} alt="Icon LinkedIn" />
-                </Social>
-              </SocialContext>
-            </Card>
-          ))}
-      </Cards>
-    </Content>
-  );
-};
+const Session = ({ data, title = 'Directors', subtitle }) => (
+  <Content>
+    <Title>{title}</Title>
+    <Function>{subtitle}</Function>
+    <Cards>
+      {data &&
+        data.map(user => (
+          <Card key={Math.random()}>
+            <Photo src={user.photo} alt={user.name} />
+            <Name>{user.name}</Name>
+            <Participation>{title.slice(0, -1)}</Participation>
+            <Description>{user.description}</Description>
+            <SocialContext>
+              <Social href={user.gitHub} target="_blank">
+                <Icon src={GitHub} alt="Icon GitHub" />
+              </Social>
+              <Social href={user.linkedIn} target="_blank">
+                <Icon src={Linkedin} alt="Icon LinkedIn" />
+              </Social>
+            </SocialContext>
+          </Card>
+        ))}
+    </Cards>
+  </Content>
+);
 
 export default Session;
